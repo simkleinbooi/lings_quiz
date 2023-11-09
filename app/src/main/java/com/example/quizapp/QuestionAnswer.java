01736bcaeff4bef6c5da4f4aa7ab8f35ca4bf7b6
sim_kleinbooi <simthembilekleinbooi@younglings.africa>
2023-11-09 12:24:41 +0200

    Last Commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "What is Alwyn van Wyk's role?",
+            "Which one is not the programming language?",
+            "What is Younglings?",
+            "Who is the social media manager at Younglins?"
+    };
+
+    public static String choices[][] = {
+            {"Co-Founder","Floor Manager","Founder","Intern"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Tech Company","Tuck Shop","Clothes Shop","Farm"},
+            {"Cameron Davids","Josha Schell","Zaakirah Abrams","Alwyn vyn Wyk"}
+    };
+
+    public static String correctAnswers[] = {
+            "Founder",
+            "Notepad",
+            "Tech Company",
+            "Zaakirah Abrams"
+    };
+
+}
